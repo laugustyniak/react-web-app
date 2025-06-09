b5520b2d016c1c695f1eeca9c929e2239daa697d
Łukasz Augustyniak <aisolutions@lukaszaugustyniak.com>
2025-06-09 13:49:20 +0000

    feat: add visually hidden dialog title and update BuyIt button text to Check Price

--- app/components/ui/dialog.tsx
+++ app/components/ui/dialog.tsx
@@ -1,8 +1,9 @@
-import type { ComponentProps } from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { XIcon } from 'lucide-react';
+import type { ComponentProps } from 'react';
 
 import { cn } from '~/lib/utils';
+import { VisuallyHidden } from './visually-hidden';
 
 function Dialog({ ...props }: ComponentProps<typeof DialogPrimitive.Root>) {
   return <DialogPrimitive.Root data-slot="dialog" {...props} />;
@@ -49,6 +50,9 @@ function DialogContent({
         )}
         {...props}
       >
+        <VisuallyHidden>
+          <DialogPrimitive.Title>Dialog</DialogPrimitive.Title>
+        </VisuallyHidden>
         {children}
         <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 cursor-pointer">
           <XIcon />
@@ -112,5 +116,6 @@ export {
   DialogOverlay,
   DialogPortal,
   DialogTitle,
-  DialogTrigger,
+  DialogTrigger
 };
+
